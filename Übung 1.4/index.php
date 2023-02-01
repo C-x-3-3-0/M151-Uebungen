086d12abcd5ef78216dc4f01b2b5eb95219eac9e
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-02-01 15:59:58 +0100

    Übung 1.4 reading out parameters

---
+++ Übung 1.4/index.php
@@ -0,0 +1,13 @@
+<?php
+    $username = $_GET['username'];
+    
+    echo "Hallo {$username}!<br />";
+
+    if ($_GET['age']) {
+        $age = $_GET['age'];
+        echo "Du bist {$age} Jahre alt.";
+    }
+
+    //index.php?username=Hao&age=19
+?>
+
